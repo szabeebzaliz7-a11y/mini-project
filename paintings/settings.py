83c4c95af6156b8d33a4f3a26540771ac7eefb25
Zabeebzaliz <szabeebzaliz7@gmail.com>
2026-01-29 17:23:45 +0530

    added .env to requirements.txt

--- paintings/settings.py
+++ paintings/settings.py
@@ -15,7 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
